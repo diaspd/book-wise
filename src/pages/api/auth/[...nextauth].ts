92168b0907b3a11520476ac9100706cebeedb17a
diaspd <pdr050203@gmail.com>
2023-08-01 00:11:08 -0300

    added login with google

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,6 +2,7 @@ import { PrismaAdapter } from "@/lib/auth/prismaAdapter";
 import { NextApiRequest, NextApiResponse } from "next";
 import NextAuth, { NextAuthOptions } from 'next-auth'
 import GithubProvider, { GithubProfile } from 'next-auth/providers/github'
+import GoogleProvider, { GoogleProfile } from 'next-auth/providers/google'
 
 export function buildNextAuthOptions(
   req: NextApiRequest,
@@ -11,6 +12,18 @@ export function buildNextAuthOptions(
     adapter: PrismaAdapter(req, res),
 
     providers: [
+      GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+        profile(profile: GoogleProfile) {
+          return {
+            id: profile.sub,
+            name: profile.name,
+            email: profile.email,
+            avatar_url: profile.picture
+          }
+        }
+      }),
       GithubProvider({
         clientId: process.env.GITHUB_CLIENT_ID!,
         clientSecret: process.env.GITHUB_CLIENT_SECRET!,
