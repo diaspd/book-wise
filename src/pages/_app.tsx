d957766024a092d1c3d6f3970d01504439ed31b9
Pedro Dias <pdr050203@gmail.com>
2024-03-30 22:49:05 -0300

    add: react tanstack query to project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,12 @@
-import { globalStyles } from '@/styles/globals'
+import { QueryClientProvider } from '@tanstack/react-query'
 import { NextPage } from 'next'
+import type { AppProps } from 'next/app'
 import { Nunito } from 'next/font/google'
 import { SessionProvider } from 'next-auth/react'
-import type { AppProps } from 'next/app'
 import { ReactElement, ReactNode } from 'react'
+
+import { queryClient } from '@/lib/react-query'
+import { globalStyles } from '@/styles/globals'
 
 export const nunito = Nunito({ subsets: ['latin'] })
 
@@ -24,10 +27,12 @@ export default function App({
   const getLayout = Component.getLayout ?? ((page) => page)
 
   return (
-    <SessionProvider session={session}>
-      <div className={nunito.className}>
-        {getLayout(<Component {...pageProps} />)}
-      </div>
-    </SessionProvider>
+    <QueryClientProvider client={queryClient}>
+      <SessionProvider session={session}>
+        <div className={nunito.className}>
+          {getLayout(<Component {...pageProps} />)}
+        </div>
+      </SessionProvider>
+    </QueryClientProvider>
   )
 }
